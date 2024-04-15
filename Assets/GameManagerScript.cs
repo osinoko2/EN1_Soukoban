044f23450a3e6dec93a8992d1e22240465b172cd
osinoko <k023g0114@g.neec.ac.jp>
2024-04-15 12:33:28 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void PrintArray()
+    {
+        string debugText = "";
+        for (int i = 0; i < map.Length; i++)
+        {
+            debugText += map[i].ToString() + ",";
+        }
+        Debug.Log(debugText);
+    }
+
+    int GetPlayerIndex()
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (map[i] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    bool MoveNumber(int number, int moveFrom, int moveTo)
+    {
+        if (moveTo < 0 || moveTo >= map.Length)
+        {
+            return false;
+        }
+        map[moveTo] = number;
+        map[moveFrom] = 0;
+        return true;
+    }
+    int[] map;
+
+    void Start()
+    {
+        // îzóÒÇÃé¿ë‘ÇÃçÏê¨Ç∆èâä˙âª
+        map = new int[] { 0,1,0,0,0,0,0,0,0 };
+        PrintArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+           
+            int playerIndex = GetPlayerIndex();
+
+            MoveNumber(1, playerIndex, playerIndex + 1);
+            PrintArray();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            int playerIndex = GetPlayerIndex();
+
+            MoveNumber(1, playerIndex, playerIndex - 1);
+            PrintArray() ;
+        }
+    }
+}
